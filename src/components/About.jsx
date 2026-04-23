86bc7eb6028a472d94ac1f69ea14976b2c45ec73
Ayush Tanty <ayushtanty003@gmail.com>
2026-04-23 13:06:50 +0530

    About added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import profile from '/profile.jpg';
+import './About.css';
 
 const About = () => {
   return (
-    <div>
-      <h1>About page</h1>
-    </div>
-  )
-}
+    <div className="about-container">
+      <div className="about-content">
+        
+        <img src={profile} alt="profile" className="profile-img" />
 
-export default About
+        <div className="about-text">
+          <h2>Ayush Tanty</h2>
+          <p>
+            I am a B.Tech student specializing in AI & ML, passionate about 
+            web development, problem solving, and building modern UI experiences.
+          </p>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default About;
